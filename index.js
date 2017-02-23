840701ed4285a8edb56f748863d32dfd9450c8b9
xuchaozheng <xuchaozheng@baidu.com>
2017-02-23 10:28:01 +0800

    init jst parser

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by xuchaozheng on 2017/2/22.
+ */
+module.exports = function (content, file, options) {
+  if(typeof content !== 'string') {
+    try {
+      content = content.toString();
+    } catch (e) {
+      throw new Error('file type is error!');
+    }
+  }
+  var fn = new Function('obj', "var p=[];with(obj||{}){p.push('" + content.replace(/[\r\t\n]/g, " ").split("<%").join("\t").replace(eval("/((^|%>)[^\\t]*)'/g"), "$1\r").replace(eval("/\\t=(.*?)%>/g"), "',$1,'").split("\t").join("');").split("%>").join("p.push('").split("\r").join("\\'") + "');}return p.join('');");
+  content = 'module.exports = ' + fn.toString();
+  console.log(content);
+  return content; // 处理后的文件内容
+}
